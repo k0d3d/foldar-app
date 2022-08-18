bbc16fee75b4bd7e3e3e6caf1a36ea9ef615cf7e
Pxlart Design <michael.rhema@gmail.com>
2022-08-18 09:48:04 +0000

    completing place order to supplier from cart

--- src/jsx/pages/cart/list-cart.tsx
+++ src/jsx/pages/cart/list-cart.tsx
@@ -3,6 +3,8 @@ import { OrderCartItems } from '../../../widgets/cart/OrderCartItems';
 
 
 function ListCartPage() {
+
+  
   return (
     <div className="row">
       <div className="col-md-10 col-lg-8 mx-auto">
